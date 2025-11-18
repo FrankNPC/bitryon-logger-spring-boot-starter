3dfcebb04e9edc6e66ad86b5610a145adfc7b80b
FrankNPC <cmings.chen@gmail.com>
2025-11-17 21:39:25 -0500

    fewfewf

---
+++ src/main/java/io/bitryon/logger/spring/LoggingProxyInitiationSpringApplicationRunListener.java
@@ -0,0 +1,19 @@
+package io.bitryon.logger.spring;
+
+import org.springframework.boot.ConfigurableBootstrapContext;
+import org.springframework.boot.SpringApplicationRunListener;
+
+/**
+ * Used to load LoggingProxyInitiation in spring META-INF/spring.factories before any classes loaded. Or can do it in main method. See bitryon-logger/README.md and bitryon-logging-integration-java-spring-example
+ */
+public class LoggingProxyInitiationSpringApplicationRunListener implements SpringApplicationRunListener {
+	
+	/**
+	 * constructor
+	 */
+	public LoggingProxyInitiationSpringApplicationRunListener() {}
+	
+	public void starting(ConfigurableBootstrapContext bootstrapContext) {
+		io.bitryon.logger.boostrap.LoggingProxyInitiation.premain(null);
+	}
+}
